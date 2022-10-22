76f48d7f6c625fe97dfeb1baaad986d377a9a23f
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-10-22 10:35:51 +0200

    first commit

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+console.debug("[ServiceWorker] Initializing");
+
+let config = {};
+
+self.addEventListener('install', function (e) {
+    console.debug('[ServiceWorker] Installing offline worker');
+    e.waitUntil(
+        fetch("./package_ed5e374f8060eb9479d5549bf6032e7c9288f04f/uno-config.js")
+            .then(r => r.text()
+                .then(configStr => {
+                    eval(configStr);
+                    caches.open('package_ed5e374f8060eb9479d5549bf6032e7c9288f04f').then(function (cache) {
+                        console.debug('[ServiceWorker] Caching app binaries and content');
+                        return cache.addAll(config.offline_files);
+                    });
+                }
+                )
+            )
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async function () {
+        try {
+            // Network first mode to get fresh content every time, then fallback to
+            // cache content if needed.
+            return await fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        }
+    }());
+});
+
+
+// managed
